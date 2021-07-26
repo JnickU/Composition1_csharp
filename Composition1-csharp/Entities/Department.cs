a12c4208829dc818dc0849e2bad83343b1ac3b45
JnickU <nicknickeado1@gmail.com>
2021-07-26 18:35:08 -0300

    Implementação total do exercicio

---
+++ Composition1-csharp/Entities/Department.cs
@@ -0,0 +1,13 @@
+﻿namespace Composition1_csharp.Entities {
+    class Department {
+        public string Name { get; set; }
+
+        public Department() {
+
+        }
+
+        public Department(string name) {
+            Name = name;
+        }
+    }
+}
